bab9a6359767ef88e8a81d64ef674d90bbdb9c8d
Matan Bakva <matanbakba@gmail.com>
2023-08-05 17:41:26 +0300

    removing dirs

--- arduino_code/arduino/arduino.ino
+++ arduino_code/arduino/arduino.ino
@@ -289,16 +289,11 @@ void sendFallRequest(){
   // Send HTTP POST request
   int httpResponseCode = http.PUT(httpRequestData);
   if (httpResponseCode>0) {
-    Serial.print("HTTP Response code: ");
-    Serial.println(httpResponseCode);
     payload = http.getString();
   }
   else {
-    Serial.print("Error code: ");
-    Serial.println(httpResponseCode);
     return;
   }
-  Serial.println(payload);
   JSONVar myObject = JSON.parse(payload);
   
   // JSON.typeof(jsonVar) can be used to get the type of the var
@@ -354,26 +349,21 @@ void checkFalling(){
    // calculating Amplitute vactor for 3 axis
    float Raw_Amp = pow(pow(ax, 2) + pow(ay, 2) + pow(az, 2), 0.5);
    int Amp = Raw_Amp * 10;  // Mulitiplied by 10 bcz values are between 0 to 1
-   Serial.println(Amp);
 if (Amp <= 2 && trigger2 == false) { //if AM breaks lower threshold (0.4g)     
 trigger1 = true;     
-Serial.println("TRIGGER 1 ACTIVATED");   
 }   
 if (trigger1 == true) {     
 trigger1count++;     
 if (Amp >= 12) { //if AM breaks upper threshold (3g)
        trigger2 = true;
-       Serial.println("TRIGGER 2 ACTIVATED");
        trigger1 = false; trigger1count = 0;
      }
    }
    if (trigger2 == true) {
      trigger2count++;
-     angleChange = pow(pow(gx, 2) + pow(gy, 2) + pow(gz, 2), 0.5); Serial.println(angleChange);
+     angleChange = pow(pow(gx, 2) + pow(gy, 2) + pow(gz, 2), 0.5); 
      if (angleChange >= 30 && angleChange <= 400) { //if orientation changes by between 80-100 degrees       
 trigger3 = true; trigger2 = false; trigger2count = 0;       
-Serial.println(angleChange);       
-Serial.println("TRIGGER 3 ACTIVATED");     
 }   
 }   
 if (trigger3 == true) {     
@@ -381,13 +371,11 @@ trigger3count++;
 if (trigger3count >= 10) {
        angleChange = pow(pow(gx, 2) + pow(gy, 2) + pow(gz, 2), 0.5);
        //delay(10);
-       Serial.println(angleChange);
        if ((angleChange >= 0) && (angleChange <= 10)) { //if orientation changes remains between 0-10 degrees         
 fall = true; trigger3 = false; trigger3count = 0;         
-Serial.println(angleChange);       }       
+}       
 else { //user regained normal orientation         
 trigger3 = false; trigger3count = 0;         
-Serial.println("TRIGGER 3 DEACTIVATED");       
 }     
 }   
 }   
@@ -398,11 +386,9 @@ fall = false;
 }   
 if (trigger2count >= 6) { //allow 0.5s for orientation change
      trigger2 = false; trigger2count = 0;
-     Serial.println("TRIGGER 2 DECACTIVATED");
    }
    if (trigger1count >= 6) { //allow 0.5s for AM to break upper threshold
      trigger1 = false; trigger1count = 0;
-     Serial.println("TRIGGER 1 DECACTIVATED");
    }
  }
 
